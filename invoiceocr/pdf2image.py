087dc8f436c2208fabe20d86dbd625e2705ec8b9
yegekucuk <eegekucuk@gmail.com>
2025-03-28 17:59:20 +0300

    refactor: delete pdf2image unused function

--- invoiceocr/pdf2image.py
+++
@@ -1,6 +0,0 @@
-from pdf2image import convert_from_path
-
-# Convert PDF to image and save each pages
-pages = convert_from_path('./images/invoice-92.pdf', 500)
-for count, page in enumerate(pages):
-    page.save(f'./images/out{count}.jpg', 'JPEG')
