a9ddba82add7fb60d42d49cfe981fffad2d0c603
yegekucuk <eegekucuk@gmail.com>
2025-05-10 22:53:52 +0300

    feat: merge ocr and get requests into a module

--- module/invoiceocr/function.py
+++ module/invoiceocr/function.py
@@ -8,11 +8,16 @@ import re
 import ollama
 from paddleocr import PaddleOCR
 
+__all__ = [
+    "read", "read_to_json", "extract_date", "extract_total",
+    "extract_items_with_llm", "to_list_of_texts"
+]
+
 # Create OCR object and disable GPU to avoid CUDA issues
 ocr = PaddleOCR(use_angle_cls=True, lang='en', use_gpu = False)
 
 # An hidden function that extracts all texts from OCR output. It should not be called directly.
-def _all_texts(extracted_list):
+def to_list_of_texts(extracted_list):
     # Merge all texts in OCR output
     all_texts = []
     for line in extracted_list:
@@ -36,7 +41,7 @@ def read(image_path:str, only_texts=False):
     extracted_list = extracted_list[0]
     # Check if only the texts are requested, return only the texts in a list
     if only_texts:
-        return _all_texts(extracted_list)
+        return to_list_of_texts(extracted_list)
     # Else return full list in the shape of [[coordinates], (text, confidence)]
     else:
         return extracted_list
@@ -61,7 +66,7 @@ def extract_date(extracted_list):
         isOnlyTexts = False
     # Concatenate into a single string, here it depends if it is only texts or not. If not, we will have to change it to only_texts
     if not isOnlyTexts: 
-        full_text = ' '.join(_all_texts(extracted_list))
+        full_text = ' '.join(to_list_of_texts(extracted_list))
     else:
         full_text = ' '.join(extracted_list)
     # Find the match
@@ -117,27 +122,13 @@ def extract_total(extracted_list):
             if amount_match:
                 return float(amount_match.group())
     # 3. Last resort: Search full text
-    # TESTS for LAST RESORT
-    # ✅ Test: 'total 23.19' -> 23.19 (Beklenen: 23.19)
-    # ✅ Test: 'Total:42.50 USD' -> 42.5 (Beklenen: 42.5)
-    # ✅ Test: 'TOTAL-15.75' -> 15.75 (Beklenen: 15.75)
-    # ✅ Test: 'tax total is 18.99' -> 18.99 (Beklenen: 18.99)
-    # ❌ Test: 'subtotal 20.00, total 22.50' -> 20.0 (Beklenen: 22.5)  # Problem!
-    # ✅ Test: 'total\n8.93' -> 8.93 (Beklenen: 8.93)
-    # ❌ Test: 'TOTALTAX0.98' -> 0.98 (Beklenen: None)  # Yanlış pozitif!
-    # ✅ Test: 'total= abc5.67' -> None (Beklenen: None)
-    # ✅ Test: 'total 123' -> None (Beklenen: None)
-    # ✅ Test: 'final total is 100.00' -> 100.0 (Beklenen: 100.0)
-    # ✅ Test: 'total' -> None (Beklenen: None)
-    # ✅ Test: 'total1.2.34' -> None (Beklenen: None)
-    # 10/12 tests
-    # -- Most of the Total prices on kaggle/images/ data set are found in this operation! --
     full_text = " ".join([item["text"] for item in ocr_data])
     last_resort_match = re.search(r"total.*?(\d+\.\d{2})", full_text, re.I)
     return float(last_resort_match.group(1)) if last_resort_match else None
 
 # Extract the items using Large Language Model, then save to a json file
 def extract_items_with_llm(extracted_list, jsonfile:str, model:str="analyzer"):
+    delimiter = "\n"
     # This function requires to check only_texts parameter of the variable 'extracted_list', we will have to check it
     isOnlyTexts:bool
     if type(extracted_list[0]) == str:
@@ -146,9 +137,9 @@ def extract_items_with_llm(extracted_list, jsonfile:str, model:str="analyzer"):
         isOnlyTexts = False
     # Concatenate into a single string, here it depends if it is only texts or not. If not, we will have to change it to only_texts
     if isOnlyTexts: 
-        text = ' '.join(extracted_list)
+        text = delimiter.join(extracted_list)
     else:
-        text = ' '.join(_all_texts(extracted_list))
+        text = delimiter.join(to_list_of_texts(extracted_list))
     # Check if parameter jsonfile is a json file
     if not jsonfile.lower().endswith(".json"):
         raise ValueError("Output file must be a JSON file")
@@ -171,3 +162,44 @@ def extract_items_with_llm(extracted_list, jsonfile:str, model:str="analyzer"):
             f.write(json_output)
     # Return the json output
     return json_output
+
+
+
+# GET IMAGES USING HTTP REQUESTS
+import requests
+
+def get_id(url):
+    return url.split("/")[-1].split("=")[1]
+
+def download_image(id, filename="receipt.jpg", csrf_token=None):
+    database_url = "https://monitoring.e-kassa.gov.az/pks-monitoring/2.0.0/documents/"
+    url = database_url + id
+    headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Connection": "keep-alive",
+        "Host": "monitoring.e-kassa.gov.az",
+        "Referer": "https://monitoring.e-kassa.gov.az/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+        "User-Lang": "en",
+        "User-Time-Zone": "Europe/Istanbul"
+    }
+
+    if csrf_token:
+        headers["X-CSRF-Token"] = csrf_token
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            with open(filename, "wb") as f:
+                f.write(response.content)
+            print(f"Image '{filename}' downloaded successfully.")
+        else:
+            print(f"Failed. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Error: {e}")
