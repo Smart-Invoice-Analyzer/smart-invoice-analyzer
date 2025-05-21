018960209c347858189b56212f1b1fbcd62aa938
yegekucuk <eegekucuk@gmail.com>
2025-05-21 12:53:00 +0300

    feat: add new functions to datasets and models submodules

--- module/datasets/__init__.py
+++ module/datasets/__init__.py
@@ -1,7 +1,43 @@
 from pandas import read_csv
 
-def amazon_sample():
-    return read_csv("./categorization/data_sample.csv")
-
-def amazon_full():
-    return read_csv("./categorization/data.csv")
+def amazon(rows=None):
+    if rows:
+        match rows:
+            case 11:
+                return read_csv("./datasets/df_11k.csv")
+            case 22:
+                return read_csv("./datasets/df_22k.csv")
+            case 55:
+                return read_csv("./datasets/df_55k.csv")
+            case 110:
+                return read_csv("./datasets/df_110k.csv")
+            case 220:
+                return read_csv("./datasets/df_220k.csv")
+            case 550:
+                return read_csv("./datasets/df_550k.csv")
+            case _:
+                return None
+    else:
+        return read_csv("./datasets/data.csv")
+    
+def amazon_summarized(rows=55):
+    if rows:
+        match rows:
+            case 11:
+                return read_csv("./datasets/summarized/df_11k_summarized.csv")
+            case 22:
+                return read_csv("./datasets/summarized/df_22k_summarized.csv")
+            case 55:
+                return read_csv("./datasets/summarized/df_55k_summarized.csv")
+            case _:
+                return None
+    else:
+        return None
+    
+def sample(df, percentage:float):
+    from sklearn.model_selection import train_test_split
+    _, df_sample = train_test_split(df, 
+                                test_size=percentage, 
+                                stratify=df['main_category'], 
+                                random_state=42)
+    return df_sample
--- module/models/__init__.py
+++ module/models/__init__.py
@@ -1,10 +1,29 @@
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from pickle import load
+    
+def train_and_eval(x, y, classes, model=None):
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import classification_report
+    from numpy import array
+    
+    if not model:
+        model = LogisticRegression()
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    print(classification_report(y_test, y_pred, target_names=array(classes)))
+    return model
 
-def load_svc(kernel:str="linear") -> SVC:
-    if kernel == "linear":
-        with open("./categorization/models/svc-linear.pkl", "rb") as file:
-            model = load(file)
-            return model
-    else: 
-        return None
+def save_model(model, name="model.pkl"):
+    import pickle
+    with open(name, 'wb') as f:
+        pickle.dump(model, f)
+
+def load_model(path):
+    import pickle
+    with open(path, 'rb') as f:
+        model = pickle.load(f)
+    return model
