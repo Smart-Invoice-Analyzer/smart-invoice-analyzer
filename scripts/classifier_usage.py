fb147951d8dac91d92b32c8b3a5c2dff54902a33
yegekucuk <eegekucuk@gmail.com>
2025-06-15 21:07:00 +0300

    feat: new model with preprocessing

--- scripts/classifier_usage.py
+++ scripts/classifier_usage.py
@@ -8,11 +8,17 @@ le: LabelEncoder
 
 cv = models.load_model("models/vectorizer.pkl")
 le = models.load_model("models/labelencoder.pkl")
+
 model = models.load_model("models/lr_full.pkl")
+
 print("Model loaded successfully")
 while True:
     string = input("Input: ")
     string = cv.transform([string])
+    nnz = string.nnz
     prediction = model.predict(string)
     class_name = le.inverse_transform(prediction)[0]
+    # If no words in bag, the class in Unknown
+    if nnz == 0:
+        class_name = "Unknown"
     print(f"Predicted class: {class_name}")
